9e169cfe5440302aae31c7dce512b9ca8cf0c112
Corrie Grimshaw <corrie@pcg.local>
2022-07-12 11:59:38 -0700

    Configure files for deployment to heroku

--- server/index.js
+++ server/index.js
@@ -13,9 +13,15 @@ if (port == null || port == "") {
   port = 3001;
 }
 
+app.use(express.static(path.join(__dirname, "dist")))
 app.use(express.static("public"))
 
+
 app.use(cors());
+
+// app.get('*', (req, res) => {
+//    //res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+// });
 
 app.get('/allMedia', (req, res)=>{
   const folder = "public/images";
